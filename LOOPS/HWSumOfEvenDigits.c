4d24a14a7627251185bf25db1e5cfb6e213831da
nadim-altaf <nadimaltaf271@gmail.com>
2026-03-31 11:17:26 +0530

    Repository cleanup

--- LOOPS/HWSumOfEvenDigits.c
+++
@@ -1,2 +0,0 @@
-// WAP to print sum of akk the even digits of a given number.
-// hint : use the same code and use if condition;
--- Strings/8_builtinFunction.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-    char *str = "Nadim"; // \0 also included in size
-    int x = strlen(str);
-    printf("%d", x);
-    return 0;
-}
